4c8100dce46c845034bb16a531e9cce7c0d85b6a
Kubat Rysbekov <quav33r@gmail.com>
2025-01-21 11:42:55 +0600

    Solve assignment_1 in lab-5

---
+++ lab-5/assignment_1.py
@@ -0,0 +1,20 @@
+from math import radians
+
+name = "Clara"
+print("Name:", name)
+
+a = 7
+b = 5
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+
+x, y = 1, 2
+x, y = y, x
+print("After swapping: x =", x, ", y =", y)
+
+PI = 3.14159
+radius = 10
+area = PI * radius ** 2
+print("Area =", area)
