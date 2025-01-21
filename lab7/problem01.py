165ca69a8cdaa14a1dd584ce2eb823992ac6bae2
Kubat Rysbekov <quav33r@gmail.com>
2025-01-21 11:51:42 +0600

    Solve assignment 1 in lab 7

---
+++ lab7/problem01.py
@@ -0,0 +1,20 @@
+# 1
+for i in range(1, 11):
+    print(i)
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# 2
+name = "Python"
+for char in name:
+    print(char)
+
+# 3
+i = 2
+while i <= 10:
+    if i % 2 == 0:
+        print(i)
+    i += 1
