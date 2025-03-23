d28c3968a0bf04fed6751c6f6287d934fe8dac75
Kubat Rysbekov <quav33r@gmail.com>
2025-03-23 23:06:30 +0600

    Solve assignments in lab 33

---
+++ lab33/problem03.js
@@ -0,0 +1,11 @@
+let name = "John";
+let age = 28;
+let isStudent = true;
+let score = null;
+let address;
+
+console.log("Name:", name, "-", "Type:", typeof name);
+console.log("Age:", age, "-", "Type:", typeof age);
+console.log("isStudent:", isStudent, "-", "Type:", typeof isStudent);
+console.log("Score:", score, "-", "Type:", typeof score);
+console.log("Address:", address, "-", "Type:", typeof address);
---
+++ lab33/problem04.js
@@ -0,0 +1,3 @@
+let userName = prompt("Enter your name:");
+let userAge = prompt("Enter your age:");
+alert("Hello, " + userName + "! You are " + userAge + " years old.");
