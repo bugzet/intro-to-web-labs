41057778dd6f14fda770e33c4be037919de04192
Kubat Rysbekov <quav33r@gmail.com>
2025-03-23 23:26:47 +0600

    Solve assignments in lab 36

---
+++ lab36/problem03.js
@@ -0,0 +1,11 @@
+if (true) {
+  var myVar = "I'm declared with var inside an if block.";
+}
+console.log(myVar);
+
+if (true) {
+  let myLet = "I'm declared with let inside an if block.";
+  const myConst = "I'm declared with const inside an if block.";
+}
+console.log(myLet);
+console.log(myConst);
